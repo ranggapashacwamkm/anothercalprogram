8d5c961056c03a7ed7330a9b9941832b4b29d49c
Rangga pc lab <r.pasha@students.amikom.ac.id>
2026-04-08 14:44:04 +0700

    Menambahkan func penambahan

--- Calculator.cs
+++ Calculator.cs
@@ -1,5 +1,20 @@
 class Calculator{
 	static void Main(string[] args) {
-	
+
+	int a = 10;
+	int b = 6;
+
+	console.WriteLine("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a, b))
+	console.WriteLine("Hasil Pengurangan : {0} + {1} = {2}", a, b, Pengurangan(a, b))
+
+
 	}
+}
+
+static int Penambahan (int a, int b){
+	return a + b;
+}
+
+static int Pengurangan(int a, int b){
+	return a + b;
 }
